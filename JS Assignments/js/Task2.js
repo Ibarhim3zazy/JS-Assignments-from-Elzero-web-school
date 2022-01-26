6d8d7e71c4a3599d0047c704d0c8d6eadb4a58d7
Ibrahim 3zazy <83598736+Ibarhim3zazy@users.noreply.github.com>
2022-01-26 16:33:45 +0200

    Add files via upload

---
+++ JS Assignments/js/Task2.js
@@ -0,0 +1,25 @@
+// Task 1
+// Normal Concatenate
+let NumberOne = 10, NumberTwo = 20;
+let result = NumberOne +""+ NumberTwo;
+ console.log(result);
+ console.log(typeof(result));
+// Template Literals Way
+let result2 = `${NumberOne}${NumberTwo}`;
+console.log(`${result2}`);
+console.log(`${typeof(result2)}`);
+// Normal Concatenate
+ console.log(NumberTwo+"\n"+NumberOne);
+// Template Literals Way
+console.log(`${NumberTwo}
+${NumberOne}`);
+// Task 2
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+// Task 3
+console.log("`I'm In \n \\\\ \n Love \\\\ \"\"\" ''' \n ++ With ++ \n \\\"\"\"\\\"\"\"\n\"\"JavaScript\"\"``");
+// Task 4
+let a = 21;
+let b = 20;
+let c = a+"_"+b
+console.log(`_${c.repeat(4)}_`); // _21_2021_2021_2021_20_
